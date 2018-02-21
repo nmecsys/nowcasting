b82d981248d787fb9c147a684c880313905442c1
daianemarcolino <daimmattos@gmail.com>
2018-02-21 10:54:44 -0300

    USGDP base 2011 banbura et al

---
+++ R/USGDPshort.R
@@ -0,0 +1,16 @@
+#' @name  USGDPshort
+#' @title Example of replication files in Banbura et al. 2011
+#' @docType data
+#' @format  A \code{list} with 2 elements: 
+#' 
+#' \itemize{
+#' \item \code{base} is a \code{mts} with 26 series and 358 observations. There are missing values;
+#' \item \code{legend} is a \code{data.frame} with specifications of the series in USGDPshort$base.
+#' }
+#' @description Dataset available to replicate the results in \emph{Banbura et al. 2011}. 
+#' @usage USGDPshort
+#' @source This dataset is available as \emph{replication files} of the seminal work \emph{Banbura et al 2011}.
+#'  One can find these \emph{replication files} in the following url: \url{https://www.newyorkfed.org/research/economists/giannone/pub}
+#' @references Banbura, M., Giannone, D. & Reichlin, L. (2011). Nowcasting. Oxford Handbook on Economic Forecasting, ed. by M. P. Clements, and D. F. Hendry, pp. 63-90. Oxford University Press.
+
+NULL
